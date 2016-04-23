b25b75d3f889c42c7660c7e425b78e018fdf2af3
ankihg <hagelin.annika@gmail.com>
2016-04-22 23:48:33 -0700

    react connect

---
+++ app/js/trees.js
@@ -0,0 +1,13 @@
+var Trees = React.createClass({
+  render: function() {
+    return (
+      <div className="trees">
+        i am trees
+      </div>
+  )}
+});
+
+ReactDOM.render(
+  <Trees />,
+  document.getElementById('trees')
+);
