5615bc497c8ad4b2454e0e521cdf26e9cff6c458
ankihg <hagelin.annika@gmail.com>
2016-04-23 00:25:02 -0700

    some data passing

--- app/js/trees.js
+++ app/js/trees.js
@@ -1,13 +1,42 @@
-var Trees = React.createClass({
+var data = [
+  {species:'livermorium lawrencia', lat: 5, lng: 1},
+  {species:'hildus mcgardica', lat: 9, lng: 4}
+];
+
+var TreeSection = React.createClass({
   render: function() {
     return (
-      <div className="trees">
+      <section className="trees">
         i am trees
+        <TreeList data={this.props.data}/>
+      </section>
+  )}
+});
+
+var TreeList = React.createClass({
+  render: function() {
+    var treeNodes = this.props.data.map(function(tree) {
+      return (
+        <Tree species={tree.species} lat={tree.lat} lng={tree.lng}>plz</Tree>
+      );
+    });
+    return (
+      <section>
+        {treeNodes}
+      </section>
+  )}
+});
+
+var Tree = React.createClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.species}
       </div>
   )}
 });
 
 ReactDOM.render(
-  <Trees />,
+  <TreeSection data={data}/>,
   document.getElementById('trees')
 );
