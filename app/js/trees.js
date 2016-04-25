866fbe2b426805429ac2ea3c6eea3c19a6d746ae
ankihg <hagelin.annika@gmail.com>
2016-04-24 20:25:15 -0700

    update tree from

--- app/js/trees.js
+++ app/js/trees.js
@@ -40,13 +40,29 @@ var TreeSection = React.createClass({
       }.bind(this)
     });
   },
-  handleDataDelete: function(id) {
+  handleDataUpdate: function(data) {
+    console.log('update tree '+data._id);
+    // $.ajax({
+    //   url: this.props.url,
+    //   type: 'PUT',
+    //   data: JSON.stringify(data),
+    //   processData: false,
+    //   success: function(res) {
+    //     this.state.data[this.state.data]
+    //     this.setState({data: });
+    //   }.bind(this),
+    //   error: function(xhr, status, err) {
+    //     console.log(this.props.url, status, err.toString());
+    //   }.bind(this)
+    // });
+  },
+  handleDataDelete: function(tree) {
     $.ajax({
-      url: this.props.url+'/'+id,
+      url: this.props.url+'/'+tree._id,
       type: 'DELETE',
       success: function(res) {
         this.setState({data: this.state.data.filter(function(data) {
-          return data.id !== id;
+          return data.id !== tree._id;
         })});
       }.bind(this),
       error: function(xhr, status, err) {
@@ -62,7 +78,7 @@ var TreeSection = React.createClass({
     return (
       <section className="trees">
         i am trees
-        <TreeList data={this.state.data} handleDataDelete={this.handleDataDelete}/>
+        <TreeList data={this.state.data} handleDataUpdate={this.handleDataUpdate} handleDataDelete={this.handleDataDelete}/>
         <TreeForm onDataSubmit={this.handleDataSubmit} />
       </section>
   )}
@@ -72,7 +88,7 @@ var TreeList = React.createClass({
   render: function() {
     var treeNodes = this.props.data.map(function(tree) {
       return (
-        <Tree treeID={tree._id} species={tree.species.cmnName} lat={tree.lat} lng={tree.lng} onDelete={this.props.handleDataDelete}>plz</Tree>
+        <Tree tree={tree} onUpdate={this.props.handleDataUpdate} onDelete={this.props.handleDataDelete}>plz</Tree>
       );
     }.bind(this));
     return (
@@ -84,16 +100,19 @@ var TreeList = React.createClass({
 
 var Tree = React.createClass({
   del: function(e) {
-    this.props.onDelete(this.props.treeID);
+    this.props.onDelete(this.props.tree);
   },
-  update: function() {
-
+  update: function(e) {
+    // attempting update
+    this.props.onUpdate(this.props.tree);
   },
   render: function() {
     return (
       <div>
-        {this.props.species} at lat: {this.props.lat} and lng: {this.props.lng} <small>id: {this.props.treeID}</small>
+        {this.props.tree.species.cmnName} at lat: {this.props.tree.lat} and lng: {this.props.tree.lng} <small>id: {this.props.tree._id}</small>
+        <button onClick={this.update}>update</button>
         <button onClick={this.del}>delete</button>
+        <UpdateTreeForm tree={this.props.tree}/>
       </div>
   )}
 });
@@ -126,6 +145,40 @@ var TreeForm = React.createClass({
         <input type="text" placeholder="species id" value={this.state.species} onChange={this.handleSpeciesChange}/> <br/>
         <input type="text" placeholder="lat" value={this.state.lat} onChange={this.handleLatChange}/> <br/>
         <input type="text" placeholder="lng" value={this.state.lng} onChange={this.handleLngChange}/>
+        <input type="submit" value="post" />
+      </form>
+  )}
+});
+
+var UpdateTreeForm = React.createClass({
+  getInitialState: function() {
+    return {species: '', lat: '', lng: ''};
+  },
+  handleSpeciesChange: function(e) {
+    this.setState({species: e.target.value});
+  },
+  handleLatChange: function(e) {
+    this.setState({lat: e.target.value});
+  },
+  handleLngChange: function(e) {
+    this.setState({lng: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var species = this.state.species.trim();
+    var lat = this.state.lat.trim();
+    var lng = this.state.lng.trim();
+    if (!species || !lat || !lng) return;
+    this.props.onDataSubmit({species:species, lat:lat, lng:lng});
+    this.setState({species: '', lat: '', lng: ''});
+  },
+  render: function() {
+    return (
+      <form className="treeForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder={this.props.tree.species._id} value={this.state.species} onChange={this.handleSpeciesChange}/> <br/>
+        <input type="text" placeholder={this.props.tree.lat}value={this.state.lat} onChange={this.handleLatChange}/> <br/>
+        <input type="text" placeholder={this.props.tree.lng} value={this.state.lng} onChange={this.handleLngChange}/>
+        <button type="cancel">cancel</button>
         <input type="submit" value="post" />
       </form>
   )}
