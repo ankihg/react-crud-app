268b5e455e84c47f20af3a24c2965f5d6e35f8ca
ankihg <hagelin.annika@gmail.com>
2016-04-25 17:13:00 -0700

    species rud

--- app/js/species.js
+++ app/js/species.js
@@ -1,11 +1,6 @@
 var serverPath = 'http://localhost:3000';
 
-var data = [
-  {species:'livermorium lawrencia', lat: 5, lng: 1},
-  {species:'hildus mcgardica', lat: 9, lng: 4}
-];
-
-var TreeSection = React.createClass({
+var SpeciesSection = React.createClass({
   getInitialState: function() {
     return {data: []}
   },
@@ -25,6 +20,7 @@ var TreeSection = React.createClass({
     })
   },
   handleDataSubmit: function(data) {
+    console.log('submit '+data);
     $.ajax({
       url: this.props.url,
       contentType: 'application/json',
@@ -33,7 +29,10 @@ var TreeSection = React.createClass({
       processData: false,
       success: function(res) {
         // this.setState({data: this.state.data.push(res)});
+        console.log('post response');
+        console.log(res);
         this.state.data.push(res);
+        console.log(this.state.data);
       }.bind(this),
       error: function(xhr, status, err) {
         console.log(this.props.url, status, err.toString());
@@ -61,13 +60,13 @@ var TreeSection = React.createClass({
       }.bind(this)
     });
   },
-  handleDataDelete: function(tree) {
+  handleDataDelete: function(species) {
     $.ajax({
-      url: this.props.url+'/'+tree._id,
+      url: this.props.url+'/'+species._id,
       type: 'DELETE',
       success: function(res) {
         this.setState({data: this.state.data.filter(function(data) {
-          return data.id !== tree._id;
+          return data.id !== species._id;
         })});
       }.bind(this),
       error: function(xhr, status, err) {
@@ -81,34 +80,34 @@ var TreeSection = React.createClass({
   },
   render: function() {
     return (
-      <section className="trees">
-        i am trees
-        <TreeList data={this.state.data} handleDataUpdate={this.handleDataUpdate} handleDataDelete={this.handleDataDelete}/>
-        <TreeForm onDataSubmit={this.handleDataSubmit} />
+      <section className="speciess">
+        i am speciess
+        <SpeciesList data={this.state.data} handleDataUpdate={this.handleDataUpdate} handleDataDelete={this.handleDataDelete}/>
+        <SpeciesForm onDataSubmit={this.handleDataSubmit} />
       </section>
   )}
 });
 
-var TreeList = React.createClass({
+var SpeciesList = React.createClass({
   render: function() {
-    var treeNodes = this.props.data.map(function(tree) {
+    var speciesNodes = this.props.data.map(function(species) {
       return (
-        <Tree tree={tree} onUpdate={this.props.handleDataUpdate} onDelete={this.props.handleDataDelete}>plz</Tree>
+        <Species species={species} onUpdate={this.props.handleDataUpdate} onDelete={this.props.handleDataDelete}>plz</Species>
       );
     }.bind(this));
     return (
       <section>
-        {treeNodes}
+        {speciesNodes}
       </section>
   )}
 });
 
-var Tree = React.createClass({
+var Species = React.createClass({
   getInitialState: function() {
     return {shoudHideUpdateFrom: true};
   },
   del: function(e) {
-    this.props.onDelete(this.props.tree);
+    this.props.onDelete(this.props.species);
   },
   showUpdateForm: function() {
     this.setState({shoudHideUpdateFrom: false});
@@ -119,79 +118,79 @@ var Tree = React.createClass({
   render: function() {
     return (
       <div>
-        {this.props.tree.species.cmnName} at lat: {this.props.tree.lat} and lng: {this.props.tree.lng} <small>[id: {this.props.tree._id}]</small>
+        {this.props.species.genus} {this.props.species.species} <small>also knowm as</small> {this.props.species.cmnName} <small>[id:{this.props.species._id}]</small>
         <button onClick={this.showUpdateForm}>update</button>
         <button onClick={this.del}>delete</button>
-        <UpdateTreeForm hide={this.hideUpdateForm} submitUpdate={this.props.onUpdate} hideFrom={this.hideUpdateForm} shouldHide={this.state.shoudHideUpdateFrom} tree={this.props.tree} />
+        <UpdateSpeciesForm hide={this.hideUpdateForm} submitUpdate={this.props.onUpdate} hideFrom={this.hideUpdateForm} shouldHide={this.state.shoudHideUpdateFrom} species={this.props.species} />
       </div>
   )}
 });
 
-var TreeForm = React.createClass({
+var SpeciesForm = React.createClass({
   getInitialState: function() {
-    return {species: '', lat: '', lng: ''};
+    return {genus: '', species: '', cmnName: ''};
+  },
+  handleGenusChange: function(e) {
+    this.setState({genus: e.target.value});
   },
   handleSpeciesChange: function(e) {
     this.setState({species: e.target.value});
   },
-  handleLatChange: function(e) {
-    this.setState({lat: e.target.value});
-  },
-  handleLngChange: function(e) {
-    this.setState({lng: e.target.value});
+  handleCmnNameChange: function(e) {
+    this.setState({cmnName: e.target.value});
   },
   handleSubmit: function(e) {
     e.preventDefault();
+    var genus = this.state.genus.trim();
     var species = this.state.species.trim();
-    var lat = this.state.lat.trim();
-    var lng = this.state.lng.trim();
-    if (!species || !lat || !lng) return;
-    this.props.onDataSubmit({species:species, lat:lat, lng:lng});
-    this.setState({species: '', lat: '', lng: ''});
+    var cmnName = this.state.cmnName.trim();
+    if (!genus || !species || !cmnName) return;
+    this.props.onDataSubmit({genus:genus, species:species, cmnName:cmnName});
+    this.setState({genus: '', species: '', cmnName: ''});
   },
   render: function() {
     return (
-      <form className="treeForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="species id" value={this.state.species} onChange={this.handleSpeciesChange}/> <br/>
-        <input type="text" placeholder="lat" value={this.state.lat} onChange={this.handleLatChange}/> <br/>
-        <input type="text" placeholder="lng" value={this.state.lng} onChange={this.handleLngChange}/>
+      <form className="speciesForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="genus" value={this.state.genus} onChange={this.handleGenusChange}/> <br/>
+        <input type="text" placeholder="species" value={this.state.species} onChange={this.handleSpeciesChange}/> <br/>
+        <input type="text" placeholder="common name" value={this.state.cmnName} onChange={this.handleCmnNameChange}/>
         <input type="submit" value="post" />
       </form>
   )}
 });
 
-var UpdateTreeForm = React.createClass({
+var UpdateSpeciesForm = React.createClass({
   getInitialState: function() {
-    return {species: this.props.tree.species._id, lat: this.props.tree.lat.toString(), lng: this.props.tree.lng.toString()};
+    return {genus: this.props.species.genus, species: this.props.species.species, cmnName: this.props.species.cmnName};
   }, cancel: function(e) {
     e.preventDefault();
-    this.setState({species: this.props.tree.species._id, lat: this.props.tree.lat, lng: this.props.tree.lng});
+    this.setState({genus: this.props.genus, species: this.props.species, cmnName: this.props.cmnName});
     this.props.hide();
+  },
+  handleGenusChange: function(e) {
+    this.setState({genus: e.target.value});
   },
   handleSpeciesChange: function(e) {
     this.setState({species: e.target.value});
   },
-  handleLatChange: function(e) {
-    this.setState({lat: e.target.value});
-  },
-  handleLngChange: function(e) {
-    this.setState({lng: e.target.value});
+  handleCmnNameChange: function(e) {
+    this.setState({cmnName: e.target.value});
   },
   handleSubmit: function(e) {
     e.preventDefault();
+    var genus = this.state.genus.trim();
     var species = this.state.species.trim();
-    var lat = this.state.lat.trim();
-    var lng = this.state.lng.trim();
-    if (!species || !lat || !lng) return;
-    this.props.submitUpdate({_id: this.props.tree._id, species:species, lat:lat, lng:lng});
-    this.setState({species: '', lat: '', lng: ''});
+    var cmnName = this.state.cmnName.trim();
+    if (!genus || !species || !cmnName) return;
+    this.props.submitUpdate({_id: this.props.species._id, genus:genus, species:species, cmnName:cmnName});
+    this.setState({genus: '', species: '', cmnName: ''});
   },
   render: function() {
     return (
       <form className={this.props.shouldHide ? 'hidden' : ''} onSubmit={this.handleSubmit}>
-        <input type="text" placeholder={this.state.species} value={this.state.species} onChange={this.handleSpeciesChange}/> <br/>
-        <input type="text" placeholder={this.state.lat}value={this.state.lat} onChange={this.handleLatChange}/> <br/>
-        <input type="text" placeholder={this.state.lng} value={this.state.lng} onChange={this.handleLngChange}/>
+        <input type="text" placeholder={this.state.genus} value={this.state.genus} onChange={this.handleGenusChange}/> <br/>
+        <input type="text" placeholder={this.state.species}value={this.state.species} onChange={this.handleSpeciesChange}/> <br/>
+        <input type="text" placeholder={this.state.cmnName} value={this.state.cmnName} onChange={this.handleCmnNameChange}/>
         <button onClick={this.cancel}>cancel</button>
         <input type="submit" value="post" onClick={this.handleSubmit}/>
       </form>
@@ -200,6 +199,6 @@ var UpdateTreeForm = React.createClass({
 
 
 ReactDOM.render(
-  <TreeSection url={serverPath+'/trees'} pollInterval={2000}/>,
-  document.getElementById('trees')
+  <SpeciesSection url={serverPath+'/speciess'} pollInterval={2000}/>,
+  document.getElementById('speciess')
 );
